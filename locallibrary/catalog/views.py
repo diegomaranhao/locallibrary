7fd76ee850d9540542ac107576bb79b9f55f8cba
Diego Maranhão <dmaranhao@gmail.com>
2018-01-11 16:55:08 -0400

    Part 5 creating index

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,22 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Ola mundo!")
+    """
+    View function for home page of site
+    """
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_author = Author.objects.all().count()
+    num_genre = Genre.objects.all().count()
+    num_python_books = Book.objects.filter(title__contains='Python').count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_author': num_author, 'num_genre': num_genre,
+                 'num_python_books': num_python_books}
+    )
